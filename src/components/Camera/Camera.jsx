9ab87ca05e26ba9b1b70c868e818ddb874af2fa8
nedcerneckis <nedcerneckis@gmail.com>
2022-01-19 18:44:11 +0000

    added facial detection

--- src/components/Camera/Camera.jsx
+++ src/components/Camera/Camera.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import * as face from 'face-api.js';
+import './Camera.css';
 
 const Camera = () => {
+    const videoHeight = 720;
+    const videoWidth = 1280;
+    const [init, setInit] = useState(false);
+    const videoRef = useRef();
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        setInit(true);
+        const MODEL_URL = `${process.env.PUBLIC_URL}/models`
+        const initModels = async () => {
+            Promise.all([
+                face.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+                face.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+                face.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+                face.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+            ]).then(startVideo);
+        }
+        initModels();
+    }, []);
+
+    const startVideo = () => {
+        navigator.mediaDevices.getUserMedia({ video: { width: 720}})
+            .then(stream => {
+                let video = videoRef.current.srcObject = stream;
+                video.play();
+            });
+    }
+
+    const handleVideoOnPlay = () => {
+        canvasRef.current.innerHTML = face.createCanvasFromMedia(videoRef.current);
+        const displaySize = {
+            width: videoWidth,
+            height: videoHeight
+        }
+        face.matchDimensions(canvasRef.current, displaySize);
+        setInterval( async () => {
+            if(init){
+                setInit(false);
+            }
+            const detections = await face.detectAllFaces(videoRef.current, new face.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+            const resizedDetections = face.resizeResults(detections, displaySize);
+            canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
+            face.draw.drawDetections(canvasRef.current, resizedDetections);
+            face.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+            face.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+        }, 200);
+    }
+
     return (
-        <video id="webcam" width="720" height="560" autoplay muted>
-        </video>
+        <div className="camera">
+                <video ref={videoRef} autoPlay muted height={videoHeight} width={videoWidth} onPlay={handleVideoOnPlay}/>
+                <canvas ref={canvasRef} />
+        </div>
     )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import AppBar from '@mui/material/AppBar';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
 import logo from '../../logo.svg';
 
 const Navbar = () => {
@@ -10,10 +10,13 @@ const Navbar = () => {
     <Stack spacing={2} sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="primary">
         <Toolbar>
-          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-            <img src={logo} className="App-logo" alt="logo" />
-            CALOR
-          </Typography>
+            <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+              <img src={logo} className="App-logo" alt="logo" />
+              CALOR
+            </Typography>
+          <Button variant="outlined">
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </Stack>
